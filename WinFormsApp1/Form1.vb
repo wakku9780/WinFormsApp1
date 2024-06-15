b456a2d1d82c681dd11ca3480b5f5482adf159b4
waqar <wakku9780@gmail.com>
2024-06-15 15:33:59 +0530

    Add project files.

---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+        MsgBox("islam here , this  Is  my Slidely task")
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+End Class
